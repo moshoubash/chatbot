3191a5297b733209123a18f89dae6019984a822c
moshoubash <ma016188@gmail.com>
2025-08-09 12:07:37 +0300

    update entry point ip

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from controllers.chat_controller import init_routes
+import os
 
 app = Flask(__name__)
 init_routes(app)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
